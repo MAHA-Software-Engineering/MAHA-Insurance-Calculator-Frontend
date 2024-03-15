4af16f50792d3c5be214b16ebfb5f8355585456c
ABaerga2002 <a.baerga2002@gmail.com>
2024-03-15 17:32:41 -0400

    add: Imported AWS Amplify to login flow

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,19 @@
 import '../app/globals.css'
+import { Amplify } from "aws-amplify"
+
+Amplify.configure({
+  Auth: {
+    verificationCodeValidity: 300, //verification code validity default 1 hrs, now set to 5 min
+    userPoolId: process.env.NEXT_PUBLIC_AWS_USER_POOL_ID,
+    region: process.env.NEXT_PUBLIC_AWS_PROJECT_REGION,
+    userPoolWebClientId: process.env.NEXT_PUBLIC_AWS_USER_POOL_APP_CLIENT_ID,
+    forgotPassword: {
+      // Limit password resets to 3 attempts per hrs
+      deliveryLimit: 3,
+      timeToLive: 60, // 60 minutes (1 hours)
+    },
+  },
+})
 
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />
