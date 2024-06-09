d20ba859732c593f6c0dab9eddc8f2672b124a44
Matthew Quijano <mq022002@gmail.com>
2024-06-09 04:27:39 -0400

    fix: Add missing viewport meta tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,11 +2,15 @@ import "../styles/globals.css";
 import "../styles/custom.css";
 import BaseLayout from "../components/layouts/BaseLayout";
 import { SessionProvider } from "../context/SessionContext";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <SessionProvider>
       <BaseLayout>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
         <Component {...pageProps} />
       </BaseLayout>
     </SessionProvider>
